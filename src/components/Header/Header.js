e11d6b3f35def0df5a41973a0e694ae7ba633e06
jessepackwood <packwoodjesse@gmail.com>
2019-04-18 15:34:55 -0600

    replace links with routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Header.css'
 import menuIcon from '../../assets/menu.svg'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import TypeWriter from '../TypeWriter/TypeWriter'
 
 const Header = ({toggleMobileMenu, menuOpened}) => {
+
   return (
     <div className='header'>
-      <h1 className='name-title'>Jesse Packwood</h1>
+      <h1 className='name-title'><Link to='/' className='route-link'>Jesse Packwood</Link></h1>
       <div className='nav-container'>
         <div className='mobile-menu'>
           <img src={menuIcon} className='menu-icon' onClick={toggleMobileMenu} />
@@ -19,10 +22,10 @@ const Header = ({toggleMobileMenu, menuOpened}) => {
             )}
         </div>
         <div className='nav-box'>
-          <a className='nav-link' href='#projects'>Projects</a>
-          {/* <a className='nav-link' href=''>Photography</a> */}
-          <a className='nav-link' href='#about'>About</a>
-          <a className='nav-link' href='#contact'>Contact</a>
+          <span className='nav-link'><Link to='projects' className='route-link'>Projects</Link></span>
+          <span className='nav-link'><Link to='/photography' className='route-link'>Photography</Link></span>
+          <span className='nav-link'><Link to='/about' className='route-link'>About</Link></span>
+          <span className='nav-link'><Link to='/contact' className='route-link'>Contact</Link></span>
         </div>
       </div>
     </div>
