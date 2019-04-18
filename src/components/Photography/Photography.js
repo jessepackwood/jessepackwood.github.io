a8cb978702269fb5e21d81861ca9ae7ca3cee327
jessepackwood <packwoodjesse@gmail.com>
2019-04-18 15:33:37 -0600

    build photography component

---
+++ src/components/Photography/Photography.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import photo from '../../assets/photos/_DSC0193.jpg'
+import './Photography.css'
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      GALLERY
+      <img src={ photo } className='umpqua-photo' />
+    </div>
+  )
+}
+
+export default Gallery
