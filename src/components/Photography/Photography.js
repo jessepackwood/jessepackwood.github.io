8ebd3be085abad4af5e79018247b787ae0d979e0
jessepackwood <packwoodjesse@gmail.com>
2019-05-17 01:32:35 -0600

    replace photography page assets

--- src/components/Photography/Photography.js
+++ src/components/Photography/Photography.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import photo from '../../assets/photos/_DSC0193.jpg'
+import photo from '../../assets/photos/kayla_buff.jpg'
 import './Photography.css'
 
 const Gallery = () => {
   return (
     <div className="gallery">
+      <h1>Photos coming soon</h1>
       <img src={ photo } className='umpqua-photo' />
     </div>
   )
