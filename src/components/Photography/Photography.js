8d50c42ba01ef0eca05f8348c6ee4b2973e45373
jessepackwood <packwoodjesse@gmail.com>
2019-06-10 14:05:19 -0600

    fix photography links

--- src/components/Photography/Photography.js
+++ src/components/Photography/Photography.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { render } from 'react-dom';
 import GridGallery from '../Gallery/Gallery'
 import './Photography.css'
 
 const Photography = () => {
   return (
     <div>
-    <GridGallery />
-    {/* <div className='mobile-gallery'>
-      <img src={ kayla_buff } className='photo' />
-      <img src={ colombia_fly } className='photo' />
-      <img src={ peacock_abel } className='photo' />
-      <img src={ janelle_royal } className='photo' />
-      <img src={ gfc_tacoma } className='photo' />
-    </div> */}
+      <GridGallery />
     </div>
   )
 }
