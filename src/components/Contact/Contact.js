543ea6670dcdca86d2036dab97851fbec8c6c5c8
jessepackwood <packwoodjesse@gmail.com>
2019-07-10 15:54:47 -0600

    add icons to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,24 @@
 import React from 'react'  
+import twitter_logo from '../../assets/logos/twitter_logo.png'
+import instagram_logo from '../../assets/logos/instagram_logo.png'
+import github_logo from '../../assets/logos/github_logo.png'
+import linkedIn_logo from '../../assets/logos/linkedIn_logo.png'
 import './Contact.css'
+
 
 const Contact = () => {
   return (
     <div className='block' id='contact'>
       <div className='subtitle-wrapper'>
         <h2 className='contact-title subtitle'>Contact</h2>
+        
       </div>
+      <section id='contact-logos'>
+          <a href='https://github.com/jessepackwood'><img src={github_logo} className='contact-logo'/></a>
+          <a href='https://www.linkedin.com/in/jessepackwood/'><img src={linkedIn_logo} className='contact-logo'/></a>
+          <a href='https://www.instagram.com/jessepackwood'><img src={instagram_logo} className='contact-logo'/></a>
+          <a href='https://www.twitter.com/jessepackwood/'><img src={twitter_logo} className='contact-logo'/></a>
+        </section>
       <div className='contact-wrapper'>
         <div className='contact-info'>
           <h3 className='contact-name'>Jesse Packwood</h3>
@@ -15,6 +27,7 @@ const Contact = () => {
           <span><a href="mailto:jesse@lokithewolfdog.com" className='contact-email-link'>jesse@lokithewolfdog.com</a></span>
         </div>
       </div>
+      
     </div>
     )
 }
